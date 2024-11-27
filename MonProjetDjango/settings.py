bec8fa1b1937e914bb71ddf7f6cb30680dfdc959
Heritsoa <152023865+Madahotel@users.noreply.github.com>
2024-11-27 08:17:54 +0300

    Installation mysql 8.0.40

--- MonProjetDjango/settings.py
+++ MonProjetDjango/settings.py
@@ -76,20 +76,21 @@ WSGI_APPLICATION = 'MonProjetDjango.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-      
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
-}
-"""
-        'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'monprojetdjango',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
+      
+   
+        }
+}
+"""'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 """
 
