d87293bec47caaad685bf2361bc4090821f44338
Heritsoa <152023865+Madahotel@users.noreply.github.com>
2024-11-27 07:45:18 +0300

    Modification django

--- MonProjetDjango/settings.py
+++ MonProjetDjango/settings.py
@@ -76,17 +76,16 @@ WSGI_APPLICATION = 'MonProjetDjango.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-   
+      
    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         }
-   
 }
 """
-   'default': {
+        'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MonProjetDjango',
+        'NAME': 'monprojetdjango',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
--- personnes/models.py
+++ personnes/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 class Personne(models.Model):
-    nom = models.CharField(max_length=50)
-    prenom = models.CharField(max_length=50)
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
     adresse = models.TextField()
     ecole = models.CharField(max_length=100)
     experience = models.TextField()
-    contact = models.CharField(max_length=20)
+    contact = models.TextField()
     diplome = models.CharField(max_length=100)
     language = models.CharField(max_length=100)
     date_naissance = models.DateField()
     genre = models.CharField(max_length=10, choices=[('M', 'Masculin'), ('F', 'Féminin')])
+    #date_creation = models.DateTimeField(auto_now_add=True)   Date de création
+    #date_modification = models.DateTimeField(auto_now=True)   Date de dernière modification
 
     def __str__(self):
         return f"{self.nom} {self.prenom}"
