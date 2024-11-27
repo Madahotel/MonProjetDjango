3fda49bead97a54cbc810971e0ddf3f0289c7400
Heritsoa <152023865+Madahotel@users.noreply.github.com>
2024-11-27 09:12:23 +0300

    reverte to sqlite 3

--- MonProjetDjango/settings.py
+++ MonProjetDjango/settings.py
@@ -77,20 +77,19 @@ WSGI_APPLICATION = 'MonProjetDjango.wsgi.application'
 
 DATABASES = {
     'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   
+        }
+}
+"""'default': {
+   
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'monprojetdjango',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
-      
-   
-        }
-}
-"""'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
     }
 """
 
